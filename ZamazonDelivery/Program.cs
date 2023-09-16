8cead9b9d2197d309ecc06ecdc6f87e337863faa
Alperen Çorak <alperen_corak@outlook.com>
2023-09-17 02:21:08 +0300

    Input controls added

--- ZamazonDelivery/Program.cs
+++ ZamazonDelivery/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Lecture_1.Part_1.InsecureBank;
 class Program
@@ -19,52 +21,118 @@ class Program
             Console.WriteLine("4. Exit");
             Console.Write("Enter your choice: ");
 
-            int choice = int.Parse(Console.ReadLine());
-
-            switch (choice)
+            int choice;
+            bool choice_control = int.TryParse(Console.ReadLine(), out choice);
+            if (choice_control)
             {
-                case 1:
-                    AddCustomer();
-                    break;
-                case 2:
-                    PlaceOrder();
-                    break;
-                case 3:
-                    ViewOrders();
-                    break;
-                case 4:
-                    Console.WriteLine("Goodbye!");
-                    return;
+                if (0 < choice && choice < 5)
+                {
+                    choice = choice;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a number between 1-4!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number.");
+            }
+
+                switch (choice)
+                {
+                    case 1:
+                        AddCustomer();
+                        break;
+                    case 2:
+                        PlaceOrder();
+                        break;
+                    case 3:
+                        ViewOrders();
+                        break;
+                    case 4:
+                        Console.WriteLine("Goodbye!");
+                        return;
+                }
             }
         }
-    }
 
-    static void AddCustomer()
-    {
-        Console.Write("Enter customer name: ");
-        string customerName = Console.ReadLine();
-        customers.Add(customerName);
-        Console.WriteLine($"Customer '{customerName}' added successfully!");
-    }
+        static void AddCustomer()
+        {
+            Console.Write("Enter customer name: ");
+            string customerName = Console.ReadLine();
+            var count = customerName.Count(char.IsLetter);
+        if (count > 0)
+        {
+            if (!char.IsLetter(customerName[0]) && count > 0)
+            {
+                Console.WriteLine("Please enter your name correct!");
+            }
+            else
+            {
+                if (customers.IndexOf(customerName) != -1)
+                {
+                    Console.WriteLine("This user already created!");
+                }
+                else
+                {
+                    customers.Add(customerName);
+                    Console.WriteLine($"Customer '{customerName}' added successfully!");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter a name!");
+        }
+        
+
+        }
 
     static void PlaceOrder()
     {
         Console.Write("Enter customer name: ");
         string customerName = Console.ReadLine();
 
-        Console.Write("Enter order details: ");
-        string orderDetails = Console.ReadLine();
-        orders.Add($"{customerName}: {orderDetails}");
-        Console.WriteLine("Order placed successfully!");
-
-    }
-
-    static void ViewOrders()
-    {
-        Console.WriteLine("Orders:");
-        foreach (var order in orders)
+        var count = customerName.Count(char.IsLetter);
+        if (count > 0)
         {
-            Console.WriteLine(order);
+            if (!char.IsLetter(customerName[0]))
+            {
+                Console.WriteLine("Please enter your name correct!");
+            }
+            else
+            {
+                if (customers.IndexOf(customerName) == -1)
+                {
+                    Console.Write("User can not found!");
+                }
+                else
+                {
+                    Console.Write("Enter order details: ");
+                    string orderDetails = Console.ReadLine();
+
+                    orders.Add($"{customerName}: {orderDetails}");
+                    Console.WriteLine("Order placed successfully!");
+
+                }
+
+            }
         }
+        else
+        {
+            Console.WriteLine("Please enter a name!");
+        }
+
     }
-}
+        
+            static void ViewOrders()
+            {
+                Console.WriteLine("Orders:");
+                foreach (var order in orders)
+                {
+                    Console.WriteLine(order);
+                }
+            }
+        
+    }
